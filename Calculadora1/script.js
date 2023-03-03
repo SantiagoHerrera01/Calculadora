aeb95a8c208590e4e38cd646da80e8632c70eaae
Santiago Castañeda Herrera <SantiagoCastaneda@comfama.com.co>
2023-03-02 20:19:10 -0500

    Sisa

---
+++ Calculadora1/script.js
@@ -0,0 +1,17 @@
+class Calculadora {
+    constructor(Calculadora){
+        this.operacion = "";
+        this.pantalla = Calculadora.queryselector("#pantalla span")
+    }
+    presionartecla(){
+        // cuando el usuario presiona una tecla
+    }
+    obtenerresultado(){
+        // cuando el usuario presiona el resultado
+    }
+    resetear(){
+
+    }
+}
+
+let calculadora = new Calculadora(document.getElementById("calculadora"))
